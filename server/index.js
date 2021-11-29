f42557779b03fc2b5dfb5d40a7d8068f2d25ce23
Katie Griffith <angel_9586@hotmail.com>
2021-11-29 15:51:50 -0600

    Testing .env

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
+
+
 
 const app = express()
+
+// dotenv.config({ path: './.env'})
 
 app.use(express.json())
 app.use(cors())
@@ -15,5 +20,8 @@ app.get(`/api/vacation`, getVacation)
 app.delete(`/api/vacation/:id`, deleteVacation)
 app.post(`/api/vacation/`, createVacation)
 app.put(`/api/vacation/:id`, updateVacation)
+
+const {SERVER_PORT} = process.env
+
 
 app.listen(5000, () => console.log('Server running on 5000'))
